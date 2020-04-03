b0f3b0bf57e81a11248cb15968b65c11e78c4632
sheen4n <sheen.an.goh@gmail.com>
2020-04-03 22:28:34 +0800

    add x-auth-token to be exposed

--- routes/users.js
+++ routes/users.js
@@ -1,64 +1,66 @@
-const bcrypt = require("bcrypt");
-const _ = require("lodash");
-const express = require("express");
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const express = require('express');
 const router = express.Router();
-const { User, validate } = require("../models/user");
-const authorization = require("../middleware/auth");
+const { User, validate } = require('../models/user');
+const authorization = require('../middleware/auth');
 
-router.get("/me", authorization, async (req, res) => {
-    const user = await User.findById(req.user._id).select("-password");
-    res.send(user);
+router.get('/me', authorization, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
 });
 
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
 
-router.post("/", async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send('User already registered');
+  try {
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 
-    const { error } = validate(req.body);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
-
-    let user = await User.findOne({ email: req.body.email });
-    if (user) return res.status(400).send("User already registered");
-    try {
-        user = new User(_.pick(req.body, ["name", "email", "password"]));
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, salt);
-
-        await user.save();
-        const token = user.generateAuthToken();
-        res.header("x-auth-token", token).send(_.pick(user, ["_id", "name", "email"]));
-
-    } catch (error) {
-        console.error(error);
-    }
-
+    await user.save();
+    const token = user.generateAuthToken();
+    res
+      .header('x-auth-token', token)
+      .header('access-control-expose-headers', 'x-auth-token')
+      .send(_.pick(user, ['_id', 'name', 'email']));
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-router.put("/:id", async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
+router.put('/:id', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    {
+      new: true,
     }
+  );
 
-    const user = await User.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
-        new: true
-    })
+  if (!user) return res.status(404).send('The user with the given ID not found');
 
-    if (!user) return res.status(404).send("The user with the given ID not found");
-
-    res.send(user);
+  res.send(user);
 });
 
-router.delete("/:id", async (req, res) => {
-    // Lookup for the Genre
-    const user = await User.findByIdAndRemove(req.params.id);
-    if (!user) {
-        return res.status(404).send("The user with the given id not found");
-    }
+router.delete('/:id', async (req, res) => {
+  // Lookup for the Genre
+  const user = await User.findByIdAndRemove(req.params.id);
+  if (!user) {
+    return res.status(404).send('The user with the given id not found');
+  }
 
-    res.send(user);
+  res.send(user);
 });
-
 
 module.exports = router;
